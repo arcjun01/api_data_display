3c154fe375a3ffaf30c455b5e4258f3200bee64b
arcjun01 <arciaga.juno@student.greenriver.edu>
2025-10-28 13:32:30 -0700

    Added a few files and implemented

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import Header from "../components/Header"
+import Footer from "../components/Footer"
+import Results from "../components/Results"
+import { useState } from "react"
+import "./App.css"
+
+export default function App() {
+  const [countries, setCountries] = useState([]);
+}
