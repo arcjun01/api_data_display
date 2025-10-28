66d6303d9d2b0ce362daec822f08824a63c1d9e5
X BELL <194058603+bellxalli@users.noreply.github.com>
2025-10-28 13:37:44 -0700

    added fetch logic working on display

---
+++ src/components/Results.jsx
@@ -0,0 +1,40 @@
+import {useState, useEffect} from "react";
+
+function Results()
+{
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    //UseEffect to get results and data from API
+    useEffect(() => {
+        async function fetchCountries(){
+            //try to get data
+            try
+            {
+                const apiResponse = await fetch("https://restcountries.com/v3.1/all");
+                if(!apiResponse.ok) //could not fetch api data
+                {
+                    throw new Error("Could not fetch data");
+                }
+
+                //otherwise everything went well
+                const results = await apiResponse.json();
+                setData(results); //store results
+            }
+            //catch error message if failed fetch
+            catch(errorMessage)
+            {
+                setError(errorMessage.message)
+            }
+            //set loading to false since fetch has been tried and done
+            finally
+            {
+                setLoading(false);
+            }
+        }
+
+        //call fetch
+        fetchCountries();
+    }, []); //[] == dependencies but there are none so it will only run once
+}
