d219d83cedf3e19836547d7e3445902d56e9f90f
arcjun01 <arciaga.juno@student.greenriver.edu>
2025-10-28 13:34:50 -0700

    modified file name

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/api-data-display/'
+  base: '/api_data_display/'
 })
